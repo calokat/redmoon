d0911675980ae7f32e313e6de764e387537c4de1
Caleb Katzenstein <caleb@calokat.me>
2023-02-05 08:34:53 -0800

    Can parse arithmetic expressions

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,274 @@
-use std::{io, borrow::BorrowMut};
+use std::io;
 
-struct Token<'a> {
-    string: &'a str,
+#[derive(PartialEq)]
+#[derive(Clone, Copy)]
+enum Token<'a> {
+    LiteralNumber(f32),
+    LiteralString(&'a str),
+    True,
+    False,
+    Nil,
+    Plus,
+    Minus,
+    Star,
+    ForwardSlash,
+    Equals,
+    NotEquals,
+    Not,
+    LeftParens,
+    RightParens
 }
 
 
-fn lexNumber(string: &str) -> (usize, Token) {
+enum Expr<'a> {
+    Binary(Box<Expr<'a>>, Token<'a>, Box<Expr<'a>>),
+    Unary(Box<Expr<'a>>, Token<'a>),
+    Literal(Token<'a>)
+}
+
+
+fn lex_number(string: &str) -> (usize, Token) {
     for (i, c) in String::from(string).chars().enumerate() {
         if c.is_numeric() || c == '.' {
              continue;
         }
-        return (i, Token {
-            string: &string[0..i],
-        });
+        return (i - 1, Token::LiteralNumber(string[0..i].parse().expect("Above code should ensure a valid parse")));
+        };
+        return (string.len() - 1, Token::LiteralNumber(string[..].parse().expect("Above code should ensure a valid parse")));
     }
-    return (string.len(), Token {
-        string,
-    });
-}
 
-fn isOperator(c: char) -> bool {
+
+fn is_operator(c: char) -> bool {
     match c {
         '+' => true,
         '-' => true,
         '/' => true,
         '*' => true,
         _ => false
+    }
+}
+
+struct Parser<'a> {
+    tokens: Vec<Token<'a>>,
+    current: usize,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(tokens: Vec<Token<'a>>) -> Self {
+        return Self { tokens, current: 0 }
+    }
+
+    pub fn primary(&mut self) -> Expr<'a> {
+        if self.check_token_type(Token::True) ||
+        self.check_token_type(Token::False) ||
+        self.check_token_type(Token::Nil) {
+            return Expr::Literal(self.previous_token());
+        }
+        if let Token::LiteralNumber(n) = self.current_token() {
+            println!("Number is {}", n);
+            let res = self.current_token();
+            self.advance();
+            return Expr::Literal(res);
+        } else if let Token::LiteralString(_) = self.current_token() {
+            let res = self.current_token();
+            self.advance();
+            return Expr::Literal(res);
+        } else if self.current_token() == Token::LeftParens {
+            self.advance();
+            let expr = self.expression();
+            
+            if !self.check_token_type(Token::RightParens) {
+                panic!("Missing right parens");
+            }
+
+            return expr;
+        }
+        unreachable!("All primary cases should have been handled");
+    }
+
+    pub fn unary(&mut self) -> Expr<'a> {
+        if self.check_token_type(Token::Minus) {
+            let operator = self.previous_token();
+            let right = self.unary();
+            return Expr::Unary(Box::new(right), operator);
+        }
+        return self.primary();
+    }
+
+    pub fn factor(&mut self) -> Expr<'a> {
+        let mut expr = self.unary();
+
+        while self.check_token_type(Token::Star) ||
+        self.check_token_type(Token::ForwardSlash) {
+            let operator = self.previous_token();
+            let right = self.unary();
+            expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
+        }
+
+        return expr;
+    }
+
+    pub fn term(&mut self) -> Expr<'a> {
+        let mut expr = self.factor();
+
+        while self.check_token_type(Token::Plus) ||
+        self.check_token_type(Token::Minus) {
+            let operator = self.previous_token();
+            let right = self.factor();
+            expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
+        }
+
+        return expr;
+    }
+
+    pub fn equality(&mut self) -> Expr<'a> {
+        let mut expr = self.term();
+
+        while self.check_token_type(Token::Equals) ||
+        self.check_token_type(Token::NotEquals) {
+            let operator = self.previous_token();
+            let right = self.term();
+            expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
+        }
+
+        return expr;
+}
+
+    pub fn expression(&mut self) -> Expr<'a> {
+        return self.equality();
+    }
+
+    fn current_token(&self) -> Token<'a> {
+        return self.tokens[self.current];
+    }
+
+    fn previous_token(&mut self) -> Token<'a> {
+        if self.current == 0 {
+            panic!("Went back before the beginning");
+        }
+
+        return self.tokens[self.current - 1];
+    }
+
+    fn advance(&mut self) {
+        if self.current < self.tokens.len() - 1 {
+            self.current += 1;
+        }
+    }
+
+    fn check_token_type(&mut self, _type: Token) -> bool {
+        let res = self.current_token() == _type;
+        if res {
+            self.advance();
+        }
+
+        res
+    }
+}
+
+fn add_exprs<'a>(t1: Token, t2: Token) -> Token<'a> {
+    if let Token::LiteralNumber(f1) = t1 {
+        if let Token::LiteralNumber(f2) = t2 {
+            println!("{}", f1 + f2);
+            return Token::LiteralNumber(f1 + f2);
+        } else if let Token::Plus = t2 {
+            panic!("Addition is not itself a number");
+        } else {
+            panic!("Well now I'm stumped");
+        }
+    } else {
+        panic!("Addition only applies to numbers, what a moron!");
+    }
+}
+
+fn subtract_exprs<'a>(t1: Token, t2: Token) -> Token<'a> {
+    if let Token::LiteralNumber(f1) = t1 {
+        match t2 {
+            Token::LiteralNumber(f2) => {
+                println!("{}", f1 - f2);
+                return Token::LiteralNumber(f1 - f2);
+            },
+            _ => panic!("Subtraction only applies to numbers")
+        }
+    } else {
+        panic!("Subtraction only applies to numbers");
+    }
+}
+
+fn multiply_exprs<'a>(t1: Token, t2: Token) -> Token<'a> {
+    if let Token::LiteralNumber(f1) = t1 {
+        match t2 {
+            Token::LiteralNumber(f2) => {
+                println!("{}", f1 * f2);
+                return Token::LiteralNumber(f1 * f2);
+            },
+            _ => panic!("Multiplication only applies to numbers")
+        }
+    } else {
+        panic!("Multiplication only applies to numbers");
+    }
+}
+
+fn divide_exprs<'a>(t1: Token, t2: Token) -> Token<'a> {
+    if let Token::LiteralNumber(f1) = t1 {
+        match t2 {
+            Token::LiteralNumber(f2) => {
+                println!("{}", f1 / f2);
+                return Token::LiteralNumber(f1 / f2);
+            },
+            _ => panic!("Division only applies to numbers")
+        }
+    } else {
+        panic!("Division only applies to numbers");
+    }
+}
+
+
+
+fn eval(expr: Expr) -> Token {
+    match expr {
+        Expr::Binary(o1, op, o2) => {
+            match op {
+                Token::Plus => {
+                    let res_op1 = eval(*o1);
+                    let res_op2 = eval(*o2);
+                    assert!(res_op2 != Token::Plus);
+                    return add_exprs(res_op1, res_op2);
+                },
+                Token::Minus => {
+                    let res_op1 = eval(*o1);
+                    let res_op2 = eval(*o2);
+                    return subtract_exprs(res_op1, res_op2);
+                },
+                Token::Star => {
+                    let res_op1 = eval(*o1);
+                    let res_op2 = eval(*o2);
+                    return multiply_exprs(res_op1, res_op2);
+                },
+                Token::ForwardSlash => {
+                    let res_op1 = eval(*o1);
+                    let res_op2 = eval(*o2);
+                    return divide_exprs(res_op1, res_op2);
+                },
+                _ => panic!("Operator not supported yet")
+            }
+        },
+        Expr::Literal(t) => {println!("I know this is a literal"); return t;},
+        Expr::Unary(e, op) => {
+            if op == Token::Minus {
+                if let Expr::Literal(t) = *e {
+                    if let Token::LiteralNumber(n) = t {
+                        return Token::LiteralNumber(-n);
+                    } else {
+                        panic!("Unsupported negation");
+                    }
+                } else {
+                    panic!("Cannot negate this");
+                }
+            } else {
+                panic!("Unsupported unary operation");
+            }
+        }
     }
 }
 
@@ -40,30 +283,37 @@ fn main() {
                     break;
                 }
                 let mut it = 0;
-                let tokens: &mut Vec<Token> = tokens.borrow_mut();
                 loop {
                     if let Some(c) = expr.chars().nth(it) {
                         if c.is_whitespace() {
                             // do nothing
                         } else if c == '(' {
+                            tokens.push(Token::LeftParens);
                         } else if c == ')' {
+                            println!("Hello right parentheses");
+                            tokens.push(Token::RightParens);
                         } else if c.is_numeric() {
-                            let (advance, tkn) = lexNumber(&expr[it..]);
+                            let (advance, tkn) = lex_number(&expr[it..]);
                             it += advance;
                             tokens.push(tkn);
-                        } else if isOperator(c) {
-                            tokens.push(Token {
-                                string:  &expr[it..it + 1]
-                            });
+                        } else if is_operator(c) {
+                            tokens.push(match c {
+                                    '+' => Token::Plus,
+                                    '-' => Token::Minus,
+                                    '/' => Token::ForwardSlash,
+                                    '*' => Token::Star,
+                                    _ => panic!("Unknown symbol")
+                                }
+                            );
                         }
                         it += 1;
                     } else {
-                        for t in tokens.into_iter() {
-                            println!("{}", t.string);
-                        }
                         break;
                     }
                 }
+                let mut parser = Parser::new(tokens);
+                let root = parser.expression();
+                eval(root);
             },
             Err(_) => {
                 println!("Error while reading input");
