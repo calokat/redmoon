c9109f475888f8641a86f9843ed7fa2c51b175f1
Caleb Katzenstein <caleb@calokat.me>
2023-02-05 11:51:17 -0800

    Support better error handling

--- src/main.rs
+++ src/main.rs
@@ -59,82 +59,98 @@ impl<'a> Parser<'a> {
         return Self { tokens, current: 0 }
     }
 
-    pub fn primary(&mut self) -> Expr<'a> {
+    pub fn primary(&mut self) -> Result<Expr<'a>, String> {
         if self.check_token_type(Token::True) ||
         self.check_token_type(Token::False) ||
         self.check_token_type(Token::Nil) {
-            return Expr::Literal(self.previous_token());
+            return Ok(Expr::Literal(self.previous_token()));
         }
         if let Token::LiteralNumber(n) = self.current_token() {
             let res = self.current_token();
             self.advance();
-            return Expr::Literal(res);
+            return Ok(Expr::Literal(res));
         } else if let Token::LiteralString(_) = self.current_token() {
             let res = self.current_token();
             self.advance();
-            return Expr::Literal(res);
+            return Ok(Expr::Literal(res));
         } else if self.current_token() == Token::LeftParens {
             self.advance();
-            let expr = Expr::Grouping(Box::new(self.expression()));
-            if !self.check_token_type(Token::RightParens) {
-                panic!("Missing right parens");
+            let expr_res = self.expression();
+            if let Ok(expr) = expr_res {
+                let expr = Expr::Grouping(Box::new(expr));
+                if !self.check_token_type(Token::RightParens) {
+                    return Err("Missing right parens".into());
+                }
+                return Ok(expr);    
             }
-
-            return expr;
+            return expr_res;
         }
-        unreachable!("All primary cases should have been handled");
+        return Err("Unknown token".into());
     }
 
-    pub fn unary(&mut self) -> Expr<'a> {
+    pub fn unary(&mut self) -> Result<Expr<'a>, String> {
         if self.check_token_type(Token::Minus) {
             let operator = self.previous_token();
-            let right = self.unary();
-            return Expr::Unary(Box::new(right), operator);
+            if let Ok(right) = self.unary() {
+                return Ok(Expr::Unary(Box::new(right), operator));
+            } else {
+                return Err("Unsupported unary operation".into())
+            }
         }
         return self.primary();
     }
 
-    pub fn factor(&mut self) -> Expr<'a> {
-        let mut expr = self.unary();
-
-        while self.check_token_type(Token::Star) ||
-        self.check_token_type(Token::ForwardSlash) {
-            let operator = self.previous_token();
-            let right = self.unary();
-            expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
+    pub fn factor(&mut self) -> Result<Expr<'a>, String> {
+        let unary = self.unary();
+        if let Ok(mut expr) = unary {
+            while self.check_token_type(Token::Star) ||
+            self.check_token_type(Token::ForwardSlash) {
+                let operator = self.previous_token();
+                let right = self.unary();
+                if let Ok(right) = right {
+                    expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
+                } else {
+                    return right;
+                }
+            }
+    
+            return Ok(expr);
+        } else {
+            return unary;
         }
-
-        return expr;
     }
 
-    pub fn term(&mut self) -> Expr<'a> {
-        let mut expr = self.factor();
+    pub fn term(&mut self) -> Result<Expr<'a>, String> {
+        let mut expr = self.factor()?;
 
         while self.check_token_type(Token::Plus) ||
         self.check_token_type(Token::Minus) {
             let operator = self.previous_token();
-            let right = self.factor();
+            let right = self.factor()?;
             expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
         }
 
-        return expr;
+        return Ok(expr);
     }
 
-    pub fn equality(&mut self) -> Expr<'a> {
-        let mut expr = self.term();
+    pub fn equality(&mut self) -> Result<Expr<'a>, String> {
+        let mut expr = self.term()?;
 
         while self.check_token_type(Token::Equals) ||
         self.check_token_type(Token::NotEquals) {
             let operator = self.previous_token();
-            let right = self.term();
+            let right = self.term()?;
             expr = Expr::Binary(Box::new(expr), operator, Box::new(right));
         }
 
-        return expr;
+        return Ok(expr);
 }
 
-    pub fn expression(&mut self) -> Expr<'a> {
-        return self.equality();
+    pub fn expression(&mut self) -> Result<Expr<'a>, String> {
+        if self.tokens.len() > 0 {
+            return self.equality();
+        }
+        return Err("No valid tokens".into());
     }
 
     fn current_token(&self) -> Token<'a> {
@@ -318,10 +334,15 @@ fn main() {
                     }
                 }
                 let mut parser = Parser::new(tokens);
-                let root = parser.expression();
-                let result = eval(root);
-                if let Token::LiteralNumber(result) = result {
-                    println!("Final evaluated number: {}", result);
+                let root_res = parser.expression();
+                if let Ok(root) = root_res {
+                    let result = eval(root);
+                    if let Token::LiteralNumber(result) = result {
+                        println!("Final evaluated number: {}", result);
+                    }    
+                } else if let Err(msg) = root_res {
+                    println!("{}", msg);
+                    continue;
                 }
             },
             Err(_) => {
